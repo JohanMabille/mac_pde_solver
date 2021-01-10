8d53b3a24d4dff3705b22be73a80379c969ddadf
meriem <m.hrittane@gmail.com>
2021-01-10 19:32:19 +0100

    Deleted the unwanted subfolder with pde_boundary_conditions definied

--- PDE/PDE/pde_boundary_conditions.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  pde_boundary_conditions.cpp
-//  PDE
-//
-//  Created by Meriem Hrittane on 10/01/2021.
-//  Copyright © 2021 Meriem Hrittane. All rights reserved.
-//
-
-#include "pde_boundary_conditions.hpp"
