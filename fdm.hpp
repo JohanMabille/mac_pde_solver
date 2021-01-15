2be6185fdcdedabefb5fd4c193a130a63bf38455
Clem <clem.ribuot@gmail.com>
2021-01-15 14:39:53 +0000

    Implementation of the FDM, still needs work on the coefficients with PDE and the rate/vol

--- fdm.hpp
+++ fdm.hpp
@@ -17,40 +17,41 @@ namespace dauphine
 			fdm_interface(); //explict?
 			virtual ~fdm_interface();
 
-			virtual double time_step(double lower_T, double upper_T, int nb_steps) const = 0;
-			virtual double space_step(double lower_T, double upper_T, int nb_steps) const = 0;
+			virtual double time_step(const double lower_T, const double upper_T, const int nb_steps) const = 0;
+			virtual double space_step(const double lower_T, const double upper_T, const int nb_steps) const = 0;
 
-			virtual double alpha(double dx, double sig); //facilite le calcul des coeffs
-			virtual double beta(double dx, double sig, double r); //facilite le calcul des coeffs
+			virtual double alpha(const double dx, const double sig) const; //facilite le calcul des coeffs
+			virtual double beta(const double dx, const double sig, const double r) const; //facilite le calcul des coeffs
+			//a adapter pour r
 
-			virtual double a1(double theta, double dt, double dx, double sig, double r);
-			virtual double a2(double theta, double dt, double dx, double sig, double r);
+			virtual double a1(const double theta, const double dt, const double dx, const double sig, const double r) const;
+			virtual double a2(const double theta, const double dt, const double dx, const double sig, const double r) const;
 
-			virtual double a3(double theta, double dt, double dx, double sig, double r);
+			virtual double a3(const double theta, const double dt, const double dx, const double sig, const  double r) const;
 
-			virtual double b1(double theta, double dt, double dx, double sig, double r);
+			virtual double b1(const double theta, const double dt, const double dx, const double sig, const double r) const;
 
-			virtual double b2(double theta, double dt, double dx, double sig, double r);
+			virtual double b2(const double theta, const double dt, const double dx, const double sig, const double r) const;
 
-			virtual double b3(double theta, double dt, double dx, double sig, double r);
+			virtual double b3(const double theta, const double dt, const double dx, const double sig, const double r) const;
 		
-			std::vector<double> old_storage;
-			std::vector<double> new_storage;
+			std::vector<double> old_storage; //necessaire?
+			std::vector<double> new_storage; //necessaire?
 
-	};
+	};//ajouter des const
 
 	class fdm : public fdm_interface
 	{
 		public:
-			fdm(pde* pde, double f0, double fN, double lower_T, double upper_T,double lower_N, double upper_N);
+			fdm(pde* pde, const double f0, const double fN, const double lower_T, const double upper_T, const double lower_N, const double upper_N);
 			~fdm();
 
-			std::vector<double> time_mesh(double lower_T, double upper_T, int dt = 1);
-			std::vector<double> space_mesh(double lower_N, double upper_N, int dx = 1);
+			std::vector<double> time_mesh(const double lower_T, const double upper_T, const int dt = 1) const;
+			std::vector<double> space_mesh(const double lower_N, const double upper_N, const int dx = 1) const;
 			
-			std::vector<double> fdm_calcul(pde* pde /*?*/);
+			std::vector<double> fdm_calcul(pde* pde /*?*/); //necessaire?
 
-			std::vector<double> thomas(std::vector<double> coeffs, std::vector<double> x, std::vector<double> d);
+			std::vector<double> thomas(const std::vector<double> coeffs, std::vector<double> x, std::vector<double> d);
 	
 
 		private:
