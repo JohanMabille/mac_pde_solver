47efd883a98f78665cdaf9177e1a45c037364ccf
Clem <clem.ribuot@gmail.com>
2021-01-18 18:48:55 +0000

    Changes of the FDM method to take into account non constant r and vol, still some unresolved errors

--- fdm.hpp
+++ fdm.hpp
@@ -14,54 +14,39 @@ namespace dauphine
 {
 	class fdm_interface
 	{
-		public:
-			fdm_interface(); //explict?
-			virtual ~fdm_interface();
-
-			virtual std::vector<double> time_mesh(const double lower_T, const double upper_T, const int nb_steps) const = 0; //utile?
-			virtual std::vector<double> space_mesh(const double lower_T, const double upper_T, const int nb_steps) const = 0;
-
-			virtual double a1(pde* pde) const;
-			virtual double a2(pde* pde) const;
-			virtual double a3(pde* pde) const;
-			virtual double b1(pde* pde) const;
-			virtual double b2(pde* pde) const;
-			virtual double b3(pde* pde) const;			
-    protected:
-        pde* m_pde;
-        payoff* m_payoff;
-        rate* r;
-        double boundary_f0;
-        double boundary_fN;
-        double uT;
-        double lT;
-        double uN;
-        double lN;
-        int m_dt = 1;
-        int m_dx;
-        double theta; 
+	public:
+		fdm_interface(); //explict?
+		virtual ~fdm_interface();
+		virtual double a1(pde* pde, double s, double t) const;
+		virtual double a2(pde* pde, double s, double t) const;
+		virtual double a3(pde* pde, double s, double t) const;			
+		virtual double b1(pde* pde, double s, double t) const;
+		virtual double b2(pde* pde, double s, double t) const;
+		virtual double b3(pde* pde, double s, double t) const;			
+    	protected:
+        	pde* m_pde;
+        	payoff* m_payoff;
+        	rate* r;
+        	double boundary_f0;
+        	double boundary_fN;
+        	int m_dt = 1;
+        	int m_dx;
+        	double theta; 
 	};
 	
 	class fdm : public fdm_interface
 	{
-		public:
-			fdm(pde* pde, payoff* payoff, const double f0, const double fN, const double lower_T, const double upper_T, const double lower_N, const double upper_N, const int dt, const int dx);
-			~fdm();
+	public:
+		fdm(pde* pde, payoff* payoff, const double f0, const double fN, const int dt, const int dx);
+		~fdm();
 
-			std::vector<double> time_mesh(const double lower_T, const double upper_T, const int dt = 1) const;
-			std::vector<double> space_mesh(const double lower_N, const double upper_N, const int dx = 1) const;
+		std::vector<double> time_mesh(const double lower_T, const double upper_T, const int dt = 1) const;
+		std::vector<double> space_mesh(const double lower_N, const double upper_N, const int dx = 1) const;
 
-			std::vector<double> thomas(const std::vector<double> coeffs, std::vector<double> x, std::vector<double> d);
-	
-
-		private:
-			
-	
+		std::vector<double> thomas(const std::vector<double> a, const std::vector<double> b, const std::vector<double> c, std::vector<double> x,  std::vector<double> d);
+		double get_price(pde* pde, interface* opt, payoff* payoff, const double f0, const double fN, const int dt, const int dx);
 	
     };
-
-
-
 
 }
 
