f760444260417fd30f8ea6593613324760dff2db
meriem <m.hrittane@gmail.com>
2021-01-29 17:15:39 +0100

    Implemented Delta value, Fixed Delta Curve, Deleted all instances of interface

--- global.hpp
+++ global.hpp
@@ -4,15 +4,15 @@
 namespace dauphine
 {
 
-    static double strike = 110.;
+
     static double initial_sigma = 0.15;
     static double initial_rate = 0.02;
-    static double spot = 120.;
+    static double spot = 100.;
     static double maturity = 7.; // in years
     static double f0 = 1.;
     static double fN = 250.;
     static double dt = 0.05;
-    static double dx = 0.3;
+    static double dx = 0.2;     //fix for smaller values
     static double theta = 0.5;
 
 }
